1a89c91721ec87f7563fdea479be3a39d54bdf3d
MarcoCorrales <mcorralese@ucenfotec.ac.cr>
2018-08-17 15:17:10 -0600

    Commit 17082018 1517
    Commit 17082018 1517
    
    Creé:
    
    Show-Hide:
    Inicio de sesión
    Registro de cliente
    GitIgnore
    CSS
    Style
    HTML
    Index.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,16 @@
-$(document).ready(function(){
+
+
+	$(document).ready(function(){
  
-	$(window).scroll(function(){
-		let barra = $(window).scrollTop(); //valor de la posicion del scroll
-		let posicion =  (barra * 0.05);
-		
-		$('body').css({
-			'background-position': '0 -' + posicion + 'px'
+		$(window).scroll(function(){
+			let barra = $(window).scrollTop(); //valor de la posicion del scroll
+			let posicion =  (barra * 0.05);
+			
+			$('body').css({
+				'background-position': '0 -' + posicion + 'px'
+			});
 		});
- 
 	});
- 
-});
+
+
+
---
+++ public/js/sistema/sistema.cotrolador.js
@@ -0,0 +1,96 @@
+
+// * * * * * * * * * * inicio: botones de prueba * * * * * * * * * * * * *
+
+// let botonIngresar =  document.querySelector('#btn_hide');
+// botonIngresar.addEventListener('click',hide);
+
+// let botonSalir = document.querySelector('#btn_show');
+// botonSalir.addEventListener('click',show);
+
+// * * * * * * * * * * fin: botones de prueba * * * * * * * * * * * * *
+
+let boton_login = document.querySelector('#btn_login');
+boton_login.addEventListener('click',show_section_login);
+
+let boton_inicio_sesion = document.querySelector('#btn_inicio_sesion');
+boton_inicio_sesion.addEventListener('click',hide_section_login);
+
+let boton_register = document.querySelector('#btn_register');
+boton_register.addEventListener('click',show_section_cliente_registro);
+
+let boton_cliente_registro = document.querySelector('#btn_cliente_registro');
+boton_cliente_registro.addEventListener('click',hide_section_cliente_registro);
+
+// * * * * * * * * * * inicio: funciones de prueba * * * * * * * * * * * * *
+// function hide(){
+//       // para que el contenedor principal se mueva según se muevan las barras del menú
+//       let contenedorPrincipal = document.querySelector('#section_cliente_registro');
+
+//       if (contenedorPrincipal != undefined) {
+//           contenedorPrincipal.classList.add('ocultar'); // * *  *probar con hasClass * * *
+//         //   contenedorPrincipal.classList.add('ocultar');
+//         botonIngresar.classList.add('ocultar');  
+//         botonSalir.classList.remove('ocultar'); 
+//       }
+// }
+// function show(){
+//     // para que el contenedor principal se mueva según se muevan las barras del menú
+//     let contenedorPrincipal = document.querySelector('#section_cliente_registro');
+
+//     if (contenedorPrincipal != undefined) {
+//         contenedorPrincipal.classList.remove('ocultar'); // * *  *probar con hasClass * * *
+//       //   contenedorPrincipal.classList.add('ocultar');
+//       botonIngresar.classList.remove('ocultar');  
+//       botonSalir.classList.add('ocultar'); 
+//     }
+// }
+// * * * * * * * * * * fin: funciones de prueba * * * * * * * * * * * * *
+
+function show_section_login(){
+
+  let contenedor = document.querySelector('#section_login');
+
+  if (contenedor != undefined) {
+      contenedor.classList.remove('ocultar');   
+    // boton_login.classList.add('ocultar'); 
+  }
+}
+
+function hide_section_login(){
+
+  let contenedor = document.querySelector('#section_login');
+
+  if (contenedor != undefined) {
+      contenedor.classList.add('ocultar');
+    // boton_login.classList.remove('ocultar'); 
+  }
+}
+
+function show_section_cliente_registro(){
+
+  let contenedor = document.querySelector('#section_cliente_registro');
+
+  if (contenedor != undefined) {
+      contenedor.classList.remove('ocultar');   
+      boton_register.classList.add('ocultar'); 
+  }
+}
+
+function hide_section_cliente_registro(){
+
+  let contenedor = document.querySelector('#section_cliente_registro');
+
+  if (contenedor != undefined) {
+      contenedor.classList.add('ocultar');
+      boton_register.classList.remove('ocultar'); 
+  }
+}
+
+
+
+
+
+
+
+
+	
